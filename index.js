6000684210701e66e9f2147781abb6442364ff56
SuzettVictorio <suzettlv18@gmail.com>
2022-08-31 22:05:53 -0600

    se eliminaron elementos anteriores para modificar estructura, se agrego color de fondo

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas =document.querySelector('canvas')
+console.log(canvas)
+const c =canvas.getContext('2d')
+console.log(canvas)
+
+canvas.width = window.innerWidth
+canvas.height = innerHeight
+
