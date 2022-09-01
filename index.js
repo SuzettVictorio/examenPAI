759c370488db419aac9fbdb79e840b652569df49
SuzettVictorio <suzettlv18@gmail.com>
2022-09-01 11:35:57 -0600

    se agregaron elementos principales

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ canvas.width = window.innerWidth
 canvas.height = innerHeight
 
 class Boundary{
+  static width =40
+  static height =40
     constructor({position}){
         this.position = position 
         this.width = 40
@@ -20,14 +22,41 @@ class Boundary{
         
     }
 }
+
+class Player{
+  constructor({position,velocity}){
+  this.position = position
+  this.velocity = velocity
+  this.radius = 15
+  }
+
+  draw(){
+    c.beginPath()
+      c.arc(this.position.x,this.position.y,this.radius,0, Math.PI*2)
+      c.fillStyle = 'yellow'
+      c.fill()
+    c.closePath()
+  }
+}
 const map = [
   ['-','-','-','-','-','-'],
   ['-',' ',' ',' ',' ','-'],
+  ['-',' ','-','-',' ','-'],
   ['-',' ',' ',' ',' ','-'],
   ['-','-','-','-','-','-'],
 ]
 
 const boundaries = []
+const player = new Player({
+  position:{
+    x:Boundary.width + Boundary.width /2,
+    y:Boundary.height + Boundary.height /2
+  },
+  velocity:{
+    x:0,
+    y:0
+  }
+})
 
 map.forEach((row,i) => {
   row.forEach((symbol,j) => {
@@ -36,8 +65,8 @@ map.forEach((row,i) => {
               boundaries.push(
                   new Boundary({
                       position:{
-                          x:40*j,
-                          y:40*i
+                          x:Boundary.width*j,
+                          y:Boundary.height*i
                       }
                   })
               )
@@ -50,7 +79,26 @@ boundaries.forEach((boundary) => {
     boundary.draw()
 })  
 
+player.draw()
 
+addEventListener('keydown',({key})=>{
+  console.log(key)
+  switch (key){
+    case 'w':
+      player.velocity.y =-5
+      break
+      case 'a':
+        player.velocity.x =-5
+        break
+      case 's':
+        player.velocity.y =5
+        break
+      case 'd':
+        player.velocity.x =5
+        break
+  }
+  console.log(player.velocity)
+})
 
 
 
